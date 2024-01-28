8e34e4fd93982110079dd17401c4cc87424f2e9f
Deepak <deepaksurya642@gmail.com>
2024-01-28 14:05:23 +0530

    added github link

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,22 +2,48 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import ChatPage from "./pages/ChatPage";
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { userAtom } from "./store/state";
 import Appbar from "./Appbar";
+import { Badge, Tag } from "@chakra-ui/react";
 
 function App() {
- 
   return (
-    <div className="App">  
+    <div className="App">
       <Router>
-        <Appbar/>
+        <Appbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/chats" element={<ChatPage />} />
         </Routes>
       </Router>
+      <Tag
+        position={"absolute"}
+        bottom={"12px"}
+        left={"12px"}
+        colorScheme="blue"
+        size="lg"
+        borderRadius="full"
+        fontFamily={"Work sans"}
+        fontSize={16}
+        
+        fontWeight={'bold'}
+      >
+        Made by&nbsp;
+        <a
+          href="https://github.com/dsmotepalli"
+          style={{ textDecoration: "underline" }}
+          target="_blank"
+        >
+          Deepak
+        </a>
+      </Tag>
     </div>
   );
 }
